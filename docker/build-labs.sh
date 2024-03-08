db79dff20ec8d9bb981e8b4995f58abe9379b027
bmorgan94 <108371356+blmorgan1994@users.noreply.github.com>
2024-03-08 11:22:57 -0600

    attempt1

--- docker/build-labs.sh
+++ docker/build-labs.sh
@@ -2,12 +2,12 @@ cd ../
 sh containthedocs-build.sh
 cd docker/
 tar czf partnerlabs.tar.gz ~/git/partnerlabs/docs/_build/html/
-sudo docker build -t leifbr/partnerlabs:latest .
+sudo docker build -t str1ngerbell/partnerlabs:latest .
 sudo docker stop partnerlabs
-sudo docker run -it --rm -d -p 80:80 --name partnerlabs leifbr/partnerlabs:latest
+sudo docker run -it --rm -d -p 80:80 --name partnerlabs str1ngerbell/partnerlabs:latest
 read -p "Push the new docker image? (y/[n]): " PUSH
 PUSH=${PUSH:-n}
 if [ $PUSH = 'y' ]
 then
-    sudo docker push leifbr/partnerlabs:latest
+    sudo docker push str1ngerbell/partnerlabs:latest
 fi
